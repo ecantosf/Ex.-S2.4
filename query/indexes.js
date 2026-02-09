e2c13ab12e993122e84446b1034030eba9885e8f
ecantosf <eduard.cantos@gmail.com>
2026-02-09 12:44:36 +0100

    fix: missing index added

--- query/indexes.js
+++ query/indexes.js
@@ -3,3 +3,6 @@ db.restaurants.createIndex({ borough: 1 })
 
 db.restaurants.createIndex({ "grades.score": 1 })
 
+db.restaurants.createIndex({ name: 1 });
+
+db.restaurants.createIndex({ cuisine: 1 });
