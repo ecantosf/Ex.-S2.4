eedc44ec40f1cb9d00c1dea447e257727104f4e2
ecantosf <eduard.cantos@gmail.com>
2026-02-09 13:24:09 +0100

    fix: fixing inde

--- query/indexes.js
+++ query/indexes.js
@@ -3,10 +3,10 @@ db.restaurants.createIndex({ borough: 1 })
 
 db.restaurants.createIndex({ "grades.score": 1 })
 
-db.restaurants.createIndex({ name: 1 });
+db.restaurants.createIndex({ name: 1 })
 
-db.restaurants.createIndex({ cuisine: 1 });
+db.restaurants.createIndex({ cuisine: 1 })
 
 db.restaurants.createIndex({"location.coordinates": "2dsphere"})
 
-db.restaurants.createIndex({ "address.street": 1 });
+db.restaurants.createIndex({ "address.street": 1 })
