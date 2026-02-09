89d99fbb79c524146634092528b6e1a6c0485b9f
ecantosf <eduard.cantos@gmail.com>
2026-02-09 13:12:32 +0100

    fix: missing index added

--- query/indexes.js
+++ query/indexes.js
@@ -6,3 +6,5 @@ db.restaurants.createIndex({ "grades.score": 1 })
 db.restaurants.createIndex({ name: 1 });
 
 db.restaurants.createIndex({ cuisine: 1 });
+
+db.restaurants.createIndex({"location.coordinates": "2dsphere"})
