69da59835241a2ed289650de4217afd437b0c206
ecantosf <eduard.cantos@gmail.com>
2026-02-09 13:26:19 +0100

    feat: show addresses without a street name

--- query/queries.js
+++ query/queries.js
@@ -80,7 +80,7 @@ db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1})
 db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1})
 
 // 28. Mostrar direccions que no contenen el carrer.
-
+db.restaurants.find({"address.street": { $in: [null, ""] }}, {_id: 0, address: 1})
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 
