e409542c9b48f035ebb152eebc8be85e06315ad2
ecantosf <eduard.cantos@gmail.com>
2026-02-09 13:15:12 +0100

    feat: query restaurants with 2nd grade A, score 9, and date 2014-08-11

--- query/queries.js
+++ query/queries.js
@@ -65,7 +65,7 @@ db.restaurants.find({"$or": [{cuisine: /Seafood/}, {name: /^Wil/}]}, {_id: 0})
 db.restaurants.find({grades: {"$elemMatch": {date: new Date("2014-08-11T00:00:00Z"), grade: "A", score: 11}}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1})
 
 // 23. Trobar restaurant_id, name i grades on el 2n element té grau "A", score 9 i data "2014-08-11T00:00:00Z".
-
+db.restaurants.find({"grades.1.date": new Date("2014-08-11T00:00:00Z"), "grades.1.grade": "A", "grades.1.score": 9}, {_id: 0, restaurant_id: 1, name: 1, grades: 1})
 
 // 24. Trobar el restaurant_id, name, street, zipcode i coordenades dels restaurants a menys de 5 km de [-74, 40.7].
 
